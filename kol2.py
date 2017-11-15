4b33761ceea143847ba94d4ae4df7eff86ece81b
Katarzyna Szydlowska <3szydlowska@taurus.fis.agh.edu.pl>
2017-11-15 18:48:34 +0100

    v1

--- kol2.py
+++ kol2.py
@@ -13,3 +13,33 @@
 # If you have enough courage and time, try storing (reading/writing) 
 # data in text files (YAML, JSON).
 # If you have even more courage, try implementing user interface.
+
+# !/usr/bin/python
+
+
+
+class Student:
+
+    def __init__(self, name, surname, nr_of_classes):
+        self.name = name
+        self.surname = surname
+        self.list_of_classes = []
+        self.classes_dictionary = {}
+        self.attendance_in_classes = [nr_of_classes]
+        self.class_nr = 0
+
+    def add_class(self, name):
+        self.classes_dictionary[name] = self.class_nr
+        self.class_nr = self.class_nr + 1
+    
+    def add_score( self, class_name, score):
+        if class_name in self.classes_dictionary.keys():
+            my_class = self.classes_dictionary[class_name]
+            print my_class
+            self.list_of_classes[int(my_class)].append(score)
+
+
+if __name__ == '__main__':
+    studentA = Student("Jan","Kowalski", 5)
+    studentA.add_class("Math")
+    studentA.add_score("Math",5.)
