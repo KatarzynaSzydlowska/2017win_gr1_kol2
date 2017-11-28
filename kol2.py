b519c13d21b5ef6522d5cd97a4a7b8a79c2e0839
Katarzyna Szydlowska <kszydlowska11@gmail.com>
2017-11-28 21:38:27 +0100

    diary v2

--- kol2.py
+++ kol2.py
@@ -1,45 +1,86 @@
-# Class diary  
-#
-# Create program for handling lesson scores.
-# Use python to handle student (highscool) class scores, and attendance.
-# Make it possible to:
-# - Get students total average score (average across classes)
-# - get students average score in class
-# - hold students name and surname
-# - Count total attendance of student
-# The default interface for interaction should be python interpreter.
-# Please, use your imagination and create more functionalities. 
-# Your project should be able to handle entire school.
-# If you have enough courage and time, try storing (reading/writing) 
-# data in text files (YAML, JSON).
-# If you have even more courage, try implementing user interface.
-
-# !/usr/bin/python
+#!/usr/bin/python
+from __future__ import division
+from collections import defaultdict
+from cmd import Cmd
+import json
 
 
+class Diary:
+    def __init__(self):
+        self.class_diary = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
 
-class Student:
+    def add_grade(self, name, surname, subject, grade):
+        self.class_diary[name + surname][subject]["Grades"].append(float(grade))
 
-    def __init__(self, name, surname, nr_of_classes):
-        self.name = name
-        self.surname = surname
-        self.list_of_classes = []
-        self.classes_dictionary = {}
-        self.attendance_in_classes = [nr_of_classes]
-        self.class_nr = 0
+    def mark_attendance(self, name, surname, subject, if_present):
+        self.class_diary[name + surname][subject]["Attendance"].append(float(if_present))
 
-    def add_class(self, name):
-        self.classes_dictionary[name] = self.class_nr
-        self.class_nr = self.class_nr + 1
-    
-    def add_score( self, class_name, score):
-        if class_name in self.classes_dictionary.keys():
-            my_class = self.classes_dictionary[class_name]
-            print my_class
-            self.list_of_classes[int(my_class)].append(score)
+    def get_average(self, my_list):
+        return sum(my_list) / len(my_list)
+
+    def get_subject_average(self, name, surname, subject):
+        return self.get_average(self.class_diary[name + surname][subject]["Grades"])
+
+    def get_total_average(self, name, surname):
+        averages = [self.get_subject_average(name, surname, subj) for subj in self.class_diary[name + surname].keys()]
+        return self.get_average(averages)
+
+    def get_total_attendance(self, name, surname):
+        attendance = [item for subj in self.class_diary[name + surname].values() for item in subj["Attendance"]]
+        return self.get_average(attendance)
+
+    def dump(self, filename):
+        with open(filename, 'w') as dump_file:
+            json.dump(self.__dict__, dump_file)
+
+
+class MyPrompt(Cmd):
+    def __init__(self, class_diary):
+        Cmd.__init__(self)
+        self.diary = class_diary
+
+    def do_a(self, line):
+        self.diary.add_grade(*line.split())
+
+    def do_b(self, line):
+        self.diary.mark_attendance(*line.split())
+
+    def do_c(self, line):
+        print self.diary.get_subject_average(*line.split())
+
+    def do_d(self, line):
+        print self.diary.get_total_average(*line.split())
+
+    def do_e(self, line):
+        print self.diary.get_total_attendance(*line.split())
+
+    def do_quit(self, args):
+        raise SystemExit
 
 
 if __name__ == '__main__':
-    studentA = Student("Jan","Kowalski", 5)
-    studentA.add_class("Math")
-    studentA.add_score("Math",5.)
+    diary = Diary()
+    diary.add_grade("Jakub", "Nowak", "Matematyka", 5.0)
+    diary.add_grade("Jakub", "Nowak", "Matematyka", 4.0)
+    diary.add_grade("Jakub", "Nowak", "Fizyka", 4.0)
+    diary.add_grade("Michal", "Nowak", "Matematyka", 5.0)
+    diary.mark_attendance("Jakub", "Nowak", "Matematyka", True)
+    diary.mark_attendance("Jakub", "Nowak", "Matematyka", True)
+    diary.mark_attendance("Jakub", "Nowak", "Matematyka", False)
+    diary.mark_attendance("Jakub", "Nowak", "Fizyka", False)
+    print diary.class_diary["JakubNowak"]["Matematyka"]["Grades"]
+    print diary.class_diary["MichalNowak"]["Matematyka"]["Grades"]
+    print diary.get_subject_average("Jakub", "Nowak", "Matematyka")
+    print diary.get_total_average("Jakub", "Nowak")
+    print diary.get_total_attendance("Jakub", "Nowak")
+    diary.dump("my_dump.json")
+
+    prompt = MyPrompt(diary)
+    prompt.prompt = '> '
+    prompt.cmdloop("""Welcome to class diary! Options:
+            a) Add grade (type: a <name> <surname> <subject> <grade>)
+            b) Mark attendance (type: b <name> <surname> <subject> <1/0>)
+            c) Get subject average (type: c <name> <surname> <subject>) 
+            d) Get total average (type: d <name> <surname>)
+            e) Get total attendance (type: e <name> <surname>) """)
+
